1550537eb5fc138c597355fae106cbb5dc609857
nishanazar <zuhaibsoomro.sst@hotmail.com>
2025-04-07 17:07:25 +0500

    45_Questions

---
+++ Q1.py
@@ -0,0 +1,9 @@
+# Personal Message: Store a person’s name in a variable, and print a message to that person.
+#  Your message should be simple, such as, “Hello Eric, would you like to learn some Python 
+# today?”
+
+person_name = "Eric"
+messsage = f"""“Hello {person_name}, would you like to learn some Python 
+today?”"""
+print(messsage)
+
